75be8daf5049920c420f87cd36e9e2309ecaf686
Vinicyos Ferreira <vinifr987@gmail.com>
2025-10-28 21:20:11 -0300

    feat: ensure multistep components work on OrdersPage

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,62 +1,50 @@
 import Header from '../components/Header';
-import Button from '../components/Button';
-import { FaMinus } from 'react-icons/fa';
 import Footer from '../components/Footer';
-import { FaPlus } from 'react-icons/fa';
-import EmptyCart from '../components/EmptyCart';
-import { FaTrash } from 'react-icons/fa';
 import { FaShoppingCart } from 'react-icons/fa';
-import {
-  REMOVE_CART,
-  INCREMENT_CART,
-  DECREMENT_CART,
-} from '../actions/cart-action';
-import { CartContext } from '../context/CartContext';
-import { useContext } from 'react';
+import { MdOutlinePayment } from 'react-icons/md';
+import Cart from '../components/Cart';
+import CustomerData from '../components/CustomerData';
+import PaymentMethod from '../components/PaymentMethod';
+import { GiNotebook } from 'react-icons/gi';
+import { useState } from 'react';
 const OrdersPage = () => {
-  const { cartState, dispatchCart } = useContext(CartContext);
+  const [step, setStep] = useState(0);
 
-  if (cartState.cart.length === 0) {
-    return <EmptyCart />;
-  }
+  const nextStep = () => {
+    setStep((stepParams) => stepParams + 1);
+  };
+
+  const prevStep = () => {
+    setStep((stepParams) => stepParams - 1);
+  };
+
+  const steps = [
+    {
+      title: 'Carrinho',
+      icon: <FaShoppingCart className="text-2xl" />,
+      component: <Cart nextStep={nextStep} />,
+    },
+    {
+      title: 'Dados do cliente',
+      icon: <GiNotebook className="text-2xl" />,
+      component: <CustomerData nextStep={nextStep} prevStep={prevStep} />,
+    },
+    {
+      title: 'Método de pagamento',
+      icon: <MdOutlinePayment className="text-2xl" />,
+      component: <PaymentMethod prevStep={prevStep} />,
+    },
+  ];
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex h-screen flex-col">
       <Header />
-      <div className="flex-1 mb-3">
-        <div className='flex my-7 items-center gap-3 justify-center'>
-          <FaShoppingCart className='text-2xl' />
-          <h2 className=" text-xl font-semibold">Carrinho</h2>
+      <div className="mb-3 flex-1">
+        <div className="my-7 flex items-center justify-center gap-3">
+          {steps[step].icon}
+          <h2 className="text-xl font-semibold">{steps[step].title}</h2>
         </div>
-        {cartState.cart &&
-          cartState.cart.map((item) => (
-            <div key={item.id} className="mx-auto flex w-[90%] gap-3 pb-7">
-              <img
-                src={item.imgUrl}
-                alt={item.name}
-                className="h-[150px] w-[200px] rounded-lg"
-              />
-              <div className="flex flex-col justify-center gap-2 text-sm">
-                <p>{item.name}</p>
-                <p>Quantidade: {item.quantity}</p>
-                <p>Preço: {item.price}R$</p>
-                <div className="flex gap-2">
-                  <Button onClick={() => INCREMENT_CART(dispatchCart, item)}>
-                    <FaPlus />
-                  </Button>
-                  <Button onClick={() => DECREMENT_CART(dispatchCart, item)}>
-                    <FaMinus />
-                  </Button>
-                  <Button onClick={() => REMOVE_CART(dispatchCart, item)}>
-                    <FaTrash />
-                  </Button>
-                </div>
-              </div>
-            </div>
-          ))}
-      </div>
-      <div className='mb-10'>
-        <h2 className="text-end text-2xl font-bold mr-5">Total: {cartState.total.toFixed(2)}R$</h2>
+        {steps[step].component}
       </div>
       <Footer />
     </div>
